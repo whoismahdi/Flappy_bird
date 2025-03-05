c89c30b79f0a9e5e2ae512dbc4f4093b62b9e264
Mahdi <mahdiseda15@gmail.com>
2025-03-05 14:06:29 +0330

    Update Game.js

--- Game.js
+++ Game.js
@@ -281,11 +281,8 @@ document.addEventListener("keypress", (e) => {
   }
 });
 
-if ("ontouchstart" in window) {
-  window.addEventListener("touchstart", gameHandler);
-} else {
-  document.addEventListener("click", gameHandler);
-}
+window.addEventListener("click", gameHandler);
+
 
 let gameBored = new bored();
 let bg = new painting(0, 0, 275, 226, 0, canvas.height - 226, true);
